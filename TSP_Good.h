c8ec6b9db4b97dc25b4da93b399c89a0d495e974
Justin Fife <justin.garrett.fife@gmail.com>
2016-04-07 21:21:58 -0600

    First Commit

---
+++ TSP_Good.h
@@ -0,0 +1,17 @@
+#ifndef _TSP_Good_h_
+#define _TSP_Good_h_
+
+#include <limits>
+#include <iostream>
+#include <map>
+#include <utility>
+#include <algorithm>
+
+class TSP
+{
+public:
+    void tsp();
+private:
+
+};
+#endif //_TSP_h_
